fc367cdf109ffb07b44cb33c359bae557b5e2cc1
DoraemonNobita001 <87823461+DoraemonNobita001@users.noreply.github.com>
2021-07-23 00:36:22 +0700

    Add files via upload

---
+++ message/help.js
@@ -0,0 +1,126 @@
+exports.menu = (prefix, i) => {
+    return `
+*</GROUP>*
+${i.emot} ${prefix}setppgrup
+${i.emot} ${prefix}opengrup
+${i.emot} ${prefix}closegrup
+${i.emot} ${prefix}join
+${i.emot} ${prefix}tagall
+${i.emot} ${prefix}mute
+${i.emot} ${prefix}unmute
+${i.emot} ${prefix}add 628xx
+${i.emot} ${prefix}kick @tag
+${i.emot} ${prefix}promote @tag
+${i.emot} ${prefix}demote @tag
+${i.emot} ${prefix}linkgc
+${i.emot} ${prefix}leave
+${i.emot} ${prefix}setdesc
+${i.emot} ${prefix}setgrupname
+${i.emot} ${prefix}antilink
+${i.emot} ${prefix}welcome
+${i.emot} ${prefix}left
+${i.emot} ${prefix}antibadword
+${i.emot} ${prefix}listbadword
+${i.emot} ${prefix}addbadword
+${i.emot} ${prefix}delbadword
+${i.emot} ${prefix}afk
+${i.emot} ${prefix}infogrup
+${i.emot} ${prefix}chatinfo
+
+*</MAKER>*
+${i.emot} ${prefix}nuliskiri text
+${i.emot} ${prefix}nuliskanan text
+${i.emot} ${prefix}foliokiri text
+${i.emot} ${prefix}foliokanan text
+${i.emot} ${prefix}blackpink text
+${i.emot} ${prefix}tahta text
+${i.emot} ${prefix}neon text
+${i.emot} ${prefix}glitch text1|text2
+${i.emot} ${prefix}thundername text
+${i.emot} ${prefix}pornhub text1|text2
+
+*</STICKER>*
+${i.emot} ${prefix}sticker
+${i.emot} ${prefix}swm
+${i.emot} ${prefix}take
+${i.emot} ${prefix}attp text
+
+*</DOWNLOAD>*
+${i.emot} ${prefix}ytmp4 url
+${i.emot} ${prefix}ytmp3 url
+${i.emot} ${prefix}igdl url
+${i.emot} ${prefix}fbdl url
+${i.emot} ${prefix}tiktok url
+${i.emot} ${prefix}yts query
+${i.emot} ${prefix}play query
+${i.emot} ${prefix}playmp4 query
+${i.emot} ${prefix}igstalk username
+${i.emot} ${prefix}ghstalk username
+
+*</GAME>*
+${i.emot} ${prefix}tictactoe @tag
+${i.emot} ${prefix}delttt
+${i.emot} ${prefix}tebakgambar
+${i.emot} ${prefix}family100
+
+*</FUN>*
+${i.emot} ${prefix}apakah
+${i.emot} ${prefix}bisakah
+${i.emot} ${prefix}kapankah
+${i.emot} ${prefix}hobby
+${i.emot} ${prefix}rate
+${i.emot} ${prefix}cekbapak
+${i.emot} ${prefix}seberapagay
+${i.emot} ${prefix}truth
+${i.emot} ${prefix}dare
+${i.emot} ${prefix}topbalance
+${i.emot} ${prefix}buylimit
+${i.emot} ${prefix}buyglimit
+${i.emot} ${prefix}cekprem
+${i.emot} ${prefix}listprem
+${i.emot} ${prefix}listban
+${i.emot} ${prefix}toimg
+${i.emot} ${prefix}tinyurl url
+${i.emot} ${prefix}imgtourl
+${i.emot} ${prefix}translate
+${i.emot} ${prefix}tagme
+${i.emot} ${prefix}kontak nomor|nama
+${i.emot} ${prefix}hidetag
+${i.emot} ${prefix}jadian
+${i.emot} ${prefix}ganteng
+${i.emot} ${prefix}cantik
+${i.emot} ${prefix}waifu
+${i.emot} ${prefix}loli
+${i.emot} ${prefix}nekonime
+${i.emot} ${prefix}megumin
+${i.emot} ${prefix}sagiri
+${i.emot} ${prefix}shinobu
+
+*</OWNER>*
+${i.emot} ${prefix}ban @tag
+${i.emot} ${prefix}unban @tag
+${i.emot} ${prefix}addprem @tag
+${i.emot} ${prefix}delprem @tag
+${i.emot} ${prefix}self
+${i.emot} ${prefix}public
+${i.emot} ${prefix}setpp
+${i.emot} ${prefix}setname
+${i.emot} ${prefix}setbio
+${i.emot} ${prefix}setprefix
+${i.emot} ${prefix}bc
+${i.emot} ${prefix}clearall
+${i.emot} ${prefix}exif nama|author
+${i.emot} >
+${i.emot} $
+
+*</ABOUT>*
+${i.emot} cekprefix
+${i.emot} ${prefix}stats
+${i.emot} ${prefix}emot
+${i.emot} ${prefix}balance
+${i.emot} ${prefix}runtime
+${i.emot} ${prefix}speed
+${i.emot} ${prefix}owner
+${i.emot} ${prefix}donasi
+${i.emot} ${prefix}sourcecode`
+}
