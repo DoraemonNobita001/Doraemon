5c42caf8a30d518c7b397192de4d8cb7d3759e02
DoraemonNobita001 <87823461+DoraemonNobita001@users.noreply.github.com>
2021-07-23 00:42:10 +0700

    Add files via upload

---
+++ message/help.js
@@ -0,0 +1,126 @@
+exports.menu = (prefix) => {
+    return `
+*</GROUP>*
+🌹 ${prefix}setppgrup
+🌹 ${prefix}opengrup
+🌹 ${prefix}closegrup
+🌹 ${prefix}join
+🌹 ${prefix}tagall
+🌹 ${prefix}mute
+🌹 ${prefix}unmute
+🌹 ${prefix}add 628xx
+🌹 ${prefix}kick @tag
+🌹 ${prefix}promote @tag
+🌹 ${prefix}demote @tag
+🌹 ${prefix}linkgc
+🌹 ${prefix}leave
+🌹 ${prefix}setdesc
+🌹 ${prefix}setgrupname
+🌹 ${prefix}antilink
+🌹 ${prefix}welcome
+🌹 ${prefix}left
+🌹 ${prefix}antibadword
+🌹 ${prefix}listbadword
+🌹 ${prefix}addbadword
+🌹 ${prefix}delbadword
+🌹 ${prefix}afk
+🌹 ${prefix}infogrup
+🌹 ${prefix}chatinfo
+
+*</MAKER>*
+🌹 ${prefix}nuliskiri text
+🌹 ${prefix}nuliskanan text
+🌹 ${prefix}foliokiri text
+🌹 ${prefix}foliokanan text
+🌹 ${prefix}blackpink text
+🌹 ${prefix}tahta text
+🌹 ${prefix}neon text
+🌹 ${prefix}glitch text1|text2
+🌹 ${prefix}thundername text
+🌹 ${prefix}pornhub text1|text2
+
+*</STICKER>*
+🌹 ${prefix}sticker
+🌹 ${prefix}swm
+🌹 ${prefix}take
+🌹 ${prefix}attp text
+
+*</DOWNLOAD>*
+🌹 ${prefix}ytmp4 url
+🌹 ${prefix}ytmp3 url
+🌹 ${prefix}igdl url
+🌹 ${prefix}fbdl url
+🌹 ${prefix}tiktok url
+🌹 ${prefix}yts query
+🌹 ${prefix}play query
+🌹 ${prefix}playmp4 query
+🌹 ${prefix}igstalk username
+🌹 ${prefix}ghstalk username
+
+*</GAME>*
+🌹 ${prefix}tictactoe @tag
+🌹 ${prefix}delttt
+🌹 ${prefix}tebakgambar
+🌹 ${prefix}family100
+
+*</FUN>*
+🌹 ${prefix}apakah
+🌹 ${prefix}bisakah
+🌹 ${prefix}kapankah
+🌹 ${prefix}hobby
+🌹 ${prefix}rate
+🌹 ${prefix}cekbapak
+🌹 ${prefix}seberapagay
+🌹 ${prefix}truth
+🌹 ${prefix}dare
+🌹 ${prefix}topbalance
+🌹 ${prefix}buylimit
+🌹 ${prefix}buyglimit
+🌹 ${prefix}cekprem
+🌹 ${prefix}listprem
+🌹 ${prefix}listban
+🌹 ${prefix}toimg
+🌹 ${prefix}tinyurl url
+🌹 ${prefix}imgtourl
+🌹 ${prefix}translate
+🌹 ${prefix}tagme
+🌹 ${prefix}kontak nomor|nama
+🌹 ${prefix}hidetag
+🌹 ${prefix}jadian
+🌹 ${prefix}ganteng
+🌹 ${prefix}cantik
+🌹 ${prefix}waifu
+🌹 ${prefix}loli
+🌹 ${prefix}nekonime
+🌹 ${prefix}megumin
+🌹 ${prefix}sagiri
+🌹 ${prefix}shinobu
+
+*</OWNER>*
+🌹 ${prefix}ban @tag
+🌹 ${prefix}unban @tag
+🌹 ${prefix}addprem @tag
+🌹 ${prefix}delprem @tag
+🌹 ${prefix}self
+🌹 ${prefix}public
+🌹 ${prefix}setpp
+🌹 ${prefix}setname
+🌹 ${prefix}setbio
+🌹 ${prefix}setprefix
+🌹 ${prefix}bc
+🌹 ${prefix}clearall
+🌹 ${prefix}exif nama|author
+🌹 >
+🌹 $
+
+*</ABOUT>*
+🌹 cekprefix
+🌹 ${prefix}stats
+🌹 ${prefix}emot
+🌹 ${prefix}balance
+🌹 ${prefix}runtime
+🌹 ${prefix}speed
+🌹 ${prefix}owner
+🌹 ${prefix}donasi
+🌹 ${prefix}sourcecode`
+}
